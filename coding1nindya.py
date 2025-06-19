d9c92a1b849d0704798d4b41b36c1bd045c171d7
Nindya <nindyamiranataalthafunnisa@gmail.com>
2025-06-19 15:51:10 +0700

    Add files via upload
    This is my first Phyton code.

---
+++ coding1nindya.py
@@ -0,0 +1,10 @@
+# Hitung Luas Persegi Panjang
+print("Halo Semuanya!")
+print("Selamat Datang di Aplikasi Hitung Persegi Panjang")
+print("="*45)
+nama = input("\nSiapa nama kamu? = ")
+print("Selamat Datang", nama)
+P = int(input("\nSilahkan Masukkan Nilai Panjang = "))
+L = int(input("Silahkan Masukkan Nilai Lebar = "))
+Luas = P*L
+print("Jadi Luasnya Adalah =", Luas, "cm²")
